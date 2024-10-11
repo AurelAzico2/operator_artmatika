6eab514e5941d573908202816824ddcd18096fc0
AURELAZICO <aurelazico981@gmail.com>
2024-10-11 09:56:16 +0700

    Add files via upload

---
+++ OperatorKeanggotaan.py
@@ -0,0 +1,21 @@
+perusahaan = 'microsoft'
+list_pulau = ['Jawa', 'Sumatra', 'Sulawesi']
+mahasiswa = {
+    'nama': 'Lendis Febri',
+    'asal': 'Lamongan'
+}
+print(
+    "Apakah 'c' ada di variabel perusahaan?",
+    'c' in perusahaan
+)
+print(
+    "Apakah 'z' tidak ada di variabel perusahaan?",
+    'c' not in perusahaan
+)
+print(
+    "Apakah 'Madura' tidak ada di variabel list_pulau?",
+)
+print(
+    "Apakah atribut 'nama' ada di variabel mahasiswa?",
+    'nama' in mahasiswa
+)
