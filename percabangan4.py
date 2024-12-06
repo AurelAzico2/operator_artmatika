ed8ac95889dc450d622e0d1c9e457df8c7a07a43
AURELAZICO <aurelazico981@gmail.com>
2024-12-06 11:41:28 +0700

    Add files via upload

---
+++ percabangan4.py
@@ -0,0 +1,6 @@
+list_buah = ['jeruk', 'mangga', 'melon', 'apel']
+liat_buah_dicari =input('Masukan nama buah dalam huruf kecil')
+if(list_buah_dicari in list_buah):
+    print('Buah ditemukan dalam list')
+else:
+    print('Buah tidak ditemukan dalam list')
---
+++ percabangan5.py
@@ -0,0 +1,12 @@
+nilai = int(input('Masukan nilai anda :'))
+usia = int(input('Masukan usia anda :'))
+if nilai >= 75:
+    if (usia < 15)
+        print('selamat adek, kamu lulus!')
+    else:
+        print('selamat kakak, kamu lulus!')
+else:
+    if(usia < 15):
+        print('Mohon maaf dek, kamu tidak lulus!')
+    else:
+        print('Mohon maaf kakak, kamu tidak lulus!')
